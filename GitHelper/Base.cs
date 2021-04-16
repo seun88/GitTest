a29b68fc796f707fb8aad5c765dc732942df4334
seunakinbode <seunakinbode@yahoo.com>
2021-04-16 20:38:36 +0100

    Add project files.

---
+++ GitHelper/Base.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest.GitHelper
+{
+    public class Base
+    {
+        public static IWebDriver driver { get; set; }
+
+
+
+
+
+
+        static Base()
+        {
+            driver = null;
+
+        }
+
+
+
+
+        public static void LaunchBrowser(String browser)
+        {
+
+
+            switch (browser)
+            {
+                case "Chrome":
+                    new DriverManager().SetUpDriver(new ChromeConfig());
+                    driver = new ChromeDriver();
+                    break;
+                case "Firefox":
+                    new DriverManager().SetUpDriver(new FirefoxConfig());
+                    driver = new FirefoxDriver();
+                    break;
+                case "InternetExplorer":
+                    new DriverManager().SetUpDriver(new InternetExplorerConfig());
+                    driver = new InternetExplorerDriver();
+                    break;
+
+                default:
+                    Console.WriteLine(browser + " is not recognised. So Firefox is launched instead");
+                    driver = new FirefoxDriver();
+                    break;
+            }
+
+            driver.Manage().Window.Maximize();
+        }
+
+        private static IWebDriver initialiseChrome()
+        {
+            driver = new ChromeDriver();
+
+            return driver;
+        }
+
+        private static IWebDriver initialiseFirefox()
+        {
+            driver = new FirefoxDriver();
+
+            return driver;
+        }
+
+        private static IWebDriver initialiseInternetExplorer()
+        {
+            driver = new InternetExplorerDriver();
+
+            return driver;
+        }
+
+        public static void CloseBrowser()
+        {
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Close();
+            driver.Quit();
+        }
+
+        public static void LaunchUrl(String Url)
+        {
+            driver.Navigate().GoToUrl(Url);
+        }
+
+
+
+
+        public static AmazonHomePage GivenINavigateAmazonPage()
+        {
+            LaunchUrl("https://www.amazon.co.uk/");
+
+            return new AmazonHomePage();
+
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("GitTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9b24d5b-e69e-4a69-8667-0f887699924a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
